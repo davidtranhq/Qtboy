91f7e74f01b0bdad1fea0548867067a6e23f39b5
hydratedcabbage <hydratedcabbage@gmail.com>
2020-03-14 00:58:18 -0400

    made timer's registers accessibile

--- include/memory.hpp
+++ include/memory.hpp
@@ -13,12 +13,14 @@
 namespace gameboy
 {
 
+class Timer;
+
 class Memory
 {
     static constexpr auto RAM_BANK_SIZE = 0x2000; // 8 KB
     public:
 
-    explicit Memory();
+    explicit Memory(Timer &t);
 
     uint8_t read(uint16_t adr) const;
     void write(uint8_t b, uint16_t adr);
@@ -36,6 +38,7 @@ class Memory
     std::array<uint8_t, 0xa0> oam_ {};
     std::array<uint8_t, 0x80> io_ {};
     std::array<uint8_t, 0x7f> hram_ {};
+    Timer &timer_; // to access hardware registers
     uint8_t ie_ {};
     bool was_written_ {false};
 
