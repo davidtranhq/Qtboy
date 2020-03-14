ecb15598af150d18820e3a267bb28e13b55e57b4
hydratedcabbage <hydratedcabbage@gmail.com>
2020-03-14 00:58:51 -0400

    Create timer.hpp

---
+++ include/timer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+
+namespace gameboy
+{
+
+class Processor;
+
+class Timer
+{
+    public:
+    Timer(Processor &p);
+
+    void update(size_t cycles);
+    uint8_t read(uint16_t adr);
+    void write(uint8_t b, uint16_t adr);
+
+    private:
+    void tima_overflow();
+
+    Processor &cpu_;
+    uint8_t tima_ {0}, tma_ {0}; // TIMA ff05, ff06
+    uint8_t tac_ {0}; // ff07
+    uint16_t div_ {0};
+
+};
+
+}
