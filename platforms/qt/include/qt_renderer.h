6bb12016246e25fa9aeea77442b214431a03f8e0
hydratedcabbage <hydratedcabbage@gmail.com>
2020-03-23 09:36:08 -0400

    render tile, scanline, framebuffer

--- platforms/qt/include/qt_renderer.h
+++ platforms/qt/include/qt_renderer.h
@@ -10,15 +10,19 @@ class Qt_renderer : public QObject, public gameboy::Renderer
     Q_OBJECT
 
     public:
-    explicit Qt_renderer(QObject *parent = nullptr);
+    explicit Qt_renderer(int w, int h, QObject *parent = nullptr);
 
-    void draw_scanline() override;
-    void draw_framebuffer(gameboy::Ppu::Frame_data) override;
+    void draw_tile(gameboy::Tile_data, size_t x, size_t y) override;
+    void draw_scanline(gameboy::Line_data) override;
+    void draw_framebuffer(gameboy::Frame_data) override;
 
     QImage image() const noexcept { return img_; }
 
     signals:
     void present_screen() override;
+
+    private:
+    void set_palette(const gameboy::Palette &p);
 
     private:
     QImage img_;
