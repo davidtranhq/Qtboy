885354d2392f56eb51d86af283b47b4e0c8ffe97
hydratedcabbage <hydratedcabbage@gmail.com>
2020-03-23 09:36:14 -0400

    added graphics

--- platforms/qt/src/mainwindow.cpp
+++ platforms/qt/src/mainwindow.cpp
@@ -2,15 +2,92 @@
 #include "mainwindow.h"
 #include "disassemblerwindow.h"
 #include "debuggerwindow.h"
+#include "vram_window.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow {parent},
-      renderer {new Qt_renderer},
+      renderer_ {new Qt_renderer(160, 144)},
       display {new QLabel}
 {
-    loadRom("../../../roms/01-special.gb");
+    system.set_renderer(renderer_);
     createActions();
+    display->setScaledContents(true);
     setCentralWidget(display);
+    connect(renderer_, SIGNAL(present_screen()), this, SLOT(update_display()));
+}
+
+
+void MainWindow::loadRom(const QString &fileName)
+{
+    system.reset();
+    system.load_cartridge(fileName.toStdString());
+    for (;;)
+    {
+        QCoreApplication::processEvents();
+        system.step(1);
+    }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+        case Qt::Key_S:
+            system.press(gameboy::Joypad::Input::B);
+            break;
+        case Qt::Key_A:
+            system.press(gameboy::Joypad::Input::A);
+            break;
+        case Qt::Key_Up:
+            system.press(gameboy::Joypad::Input::Up);
+            break;
+        case Qt::Key_Down:
+            system.press(gameboy::Joypad::Input::Down);
+            break;
+        case Qt::Key_Left:
+            system.press(gameboy::Joypad::Input::Left);
+            break;
+        case Qt::Key_Right:
+            system.press(gameboy::Joypad::Input::Right);
+            break;
+        case Qt::Key_Shift:
+            system.press(gameboy::Joypad::Input::Select);
+            break;
+        case Qt::Key_Return:
+            system.press(gameboy::Joypad::Input::Start);
+            break;
+    }
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+        case Qt::Key_S:
+            system.release(gameboy::Joypad::Input::B);
+            break;
+        case Qt::Key_A:
+            system.release(gameboy::Joypad::Input::A);
+            break;
+        case Qt::Key_Up:
+            system.release(gameboy::Joypad::Input::Up);
+            break;
+        case Qt::Key_Down:
+            system.release(gameboy::Joypad::Input::Down);
+            break;
+        case Qt::Key_Left:
+            system.release(gameboy::Joypad::Input::Left);
+            break;
+        case Qt::Key_Right:
+            system.release(gameboy::Joypad::Input::Right);
+            break;
+        case Qt::Key_Shift:
+            system.release(gameboy::Joypad::Input::Select);
+            break;
+        case Qt::Key_Return:
+            system.release(gameboy::Joypad::Input::Start);
+            break;
+    }
 }
 
 void MainWindow::openRom()
@@ -32,10 +109,23 @@ void MainWindow::showDebugger()
     debugger->show();
 }
 
+void MainWindow::showVramViewer()
+{
+    auto vram_viewer = new Vram_window(&system);
+    vram_viewer->show();
+}
+
+
 void MainWindow::about()
 {
     QMessageBox::about(this, tr("About QtBoy"),
                        tr("A Gameboy emulator made using Qt."));
+}
+
+void MainWindow::update_display()
+{
+    QImage img {renderer_->image()};
+    display->setPixmap(QPixmap::fromImage(img));
 }
 
 void MainWindow::createActions()
@@ -47,13 +137,8 @@ void MainWindow::createActions()
     QMenu *toolsMenu = menuBar()->addMenu(tr("&Tools"));
     QAction *disassembleAct = toolsMenu->addAction(tr("Disassemble"), this, &MainWindow::showDisassembler);
     QAction *debuggerAct = toolsMenu->addAction(tr("Debugger"), this, &MainWindow::showDebugger);
+    QAction *vramViewerAct = toolsMenu->addAction(tr("VRAM Viewer"), this, &MainWindow::showVramViewer);
 
     QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
     QAction *aboutAct = helpMenu->addAction(tr("&About"), this, &MainWindow::about);
-}
-
-void MainWindow::loadRom(const QString &fileName)
-{
-    system.reset();
-    system.load_cartridge(fileName.toStdString());
 }
