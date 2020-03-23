4d42d60c7c6ab88b357366d81ff5524c2d3b0bff
hydratedcabbage <hydratedcabbage@gmail.com>
2020-03-23 09:32:33 -0400

    added access to PPU and Joypad registers

--- include/memory.hpp
+++ include/memory.hpp
@@ -3,7 +3,6 @@
 #include <array>
 #include <optional>
 #include <cstdint>
-#include <stdexcept>
 #include <vector>
 #include <map>
 
@@ -14,13 +13,15 @@ namespace gameboy
 {
 
 class Timer;
+class Ppu;
+class Joypad;
 
 class Memory
 {
     static constexpr auto RAM_BANK_SIZE = 0x2000; // 8 KB
     public:
 
-    explicit Memory(Timer &t);
+    explicit Memory(Ppu &p, Timer &t, Joypad &j);
 
     uint8_t read(uint16_t adr) const;
     void write(uint8_t b, uint16_t adr);
@@ -32,18 +33,22 @@ class Memory
     bool was_written();
 
     private:
+    void set_ram_size();
+    void init_io();
+    void dma_transfer(uint8_t b);
+
+    private:
     std::optional<Cartridge> cart_ {std::nullopt};
     Video_ram vram_ {}; // 8KB x 2
     Work_ram wram_ {};
     std::array<uint8_t, 0xa0> oam_ {};
     std::array<uint8_t, 0x80> io_ {};
     std::array<uint8_t, 0x7f> hram_ {};
+    Ppu &ppu_; // to access hardware registers
     Timer &timer_; // to access hardware registers
+    Joypad &joypad_; // to access hardware registers
     uint8_t ie_ {};
     bool was_written_ {false};
-
-    void set_ram_size();
-    void init_io();
 
 };
 
